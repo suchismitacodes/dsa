a49d23dd2286249ae8973222ea2087cc78b3ae75
Suchismita Bose <suchismitabose29@gmail.com>
2026-03-13 04:41:45 +0530

    Create Subarrays_with_sum_K_560.java

---
+++ Array/Subarrays_with_sum_K_560.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0,1); // <Prefix Sum, Frequency>
+        int sum=0, count=0;
+        for(int i=0; i<nums.length; i++) {
+            sum += nums[i]; // Prefix Sum
+            if(map.containsKey(sum-k))
+                count += map.get(sum-k);
+            map.put(sum, map.getOrDefault(sum, 0)+1);          
+        }
+        return count;
+    }
+}
