3c7ba0140f580c27b4de17aace910a602011bfe3
Suchismita Bose <suchismitabose29@gmail.com>
2026-03-13 03:50:38 +0530

    Create Missing_Number_Within _Range_268.java

---
+++ Array/Missing_Number_Within _Range_268.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n=nums.length;
+        int expectedSum = n*(n+1)/2;
+        int sum=0;
+        for(int i:nums)
+            sum+=i;
+        return expectedSum - sum;
+    }
+}
