2c02d819a5e4534f4223b54cc9371399d9156731
Suchismita Bose <suchismitabose29@gmail.com>
2026-03-13 01:41:28 +0530

    Implement method to find the largest element in array

---
+++ Array/Largest_Element.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int largestElement(int[] nums) {
+        int max=nums[0];
+        for(int i=1; i<nums.length; i++) {
+            if(nums[i] > max)
+                max=nums[i];
+        }
+        return max;
+    }
+}
