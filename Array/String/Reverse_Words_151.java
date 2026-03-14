21bdd7fefaf6ef32ddc7e9099d102022a13691b9
Suchismita Bose <suchismitabose29@gmail.com>
2026-03-14 06:50:26 +0530

    Create Reverse_Words_151.java

---
+++ Array/String/Reverse_Words_151.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String reverseWords(String s) {
+        String words[] = s.trim().split("\\s+");
+        StringBuilder res=new StringBuilder();
+        for(int i=words.length-1; i>=0; i--)
+            res.append(words[i]).append(" ");
+        return res.toString().trim();
+    }
+}
